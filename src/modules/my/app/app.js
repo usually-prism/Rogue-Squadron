09920ceb28b6dfcb497af445eb3e5396772d25b8
Charles Bowman <72181341+usually-prism@users.noreply.github.com>
2021-10-18 16:54:27 -0500

    Added Conditional Rendering

--- src/modules/my/app/app.js
+++ src/modules/my/app/app.js
@@ -2,4 +2,23 @@ import { LightningElement } from 'lwc';
 
 export default class App extends LightningElement {
 
+    clickedWet = true;
+    clickedBrush = false;
+    clickedHolder = false;
+
+    handleWet() {
+        this.clickedWet = true;
+        this.clickedBrush = false;
+        this.clickedHolder = false;
+    }
+    handleBrush() {
+        this.clickedWet = false;
+        this.clickedBrush = true;
+        this.clickedHolder = false;
+    }
+    handleHandle() {
+        this.clickedWet = false;
+        this.clickedBrush = false;
+        this.clickedHolder = true;
+    }
 }
